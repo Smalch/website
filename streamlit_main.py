a6e8b4698435227afc312958ac236445d6022baa
Smalch <smalch1234@gmail.com>
2023-06-12 15:58:38 +0200

    Bug fix long requests

--- streamlit_main.py
+++ streamlit_main.py
@@ -64,10 +64,13 @@ def main():
                 f"{url}/qa_from_files/",
                 json={"query": input},
             )
-            if response.status_code != 200:
+            try:
+                if response.status_code != 200:
+                    answer = last_answer()
+                else:
+                    answer = response.json()
+            except:
                 answer = last_answer()
-            else:
-                answer = response.json()
         print(answer)
         st.session_state.history.append(answer)
         message(answer, key=str(len(st.session_state.history)))
