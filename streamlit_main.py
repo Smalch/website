761e0da278daa98546603eca8102c05efbd6fde1
Smalch <smalch1234@gmail.com>
2023-06-14 11:24:47 +0200

    Fixed text deliting

--- streamlit_main.py
+++ streamlit_main.py
@@ -74,7 +74,7 @@ def main():
                     )
 
     input = st.session_state.q
-    if input:
+    if input and input != '':
         setup_history(st.session_state.history)
         message(input, is_user=True, key=str(len(st.session_state.history)))
         st.session_state.history.append(input)
